88ac38e42ef213e9493768935b68e15bb7c8705b
Richard Damon Ciampa <ciam1324@mlc104.csumb.edu>
2016-01-27 22:37:31 -0800

    Lab1 Fall 2015

---
+++ lab1/Scripts/js/main.js
@@ -0,0 +1,9 @@
+//potusExitGlacierVideo = "//www.youtube-nocookie.com/embed/sarbtnJL31Q";
+//putusSewardHarbor = "//www.youtube-nocookie.com/embed/DTAtnG8XaNY";
+//potusAlaskaNatives = "//www.youtube-nocookie.com/embed/fJl3JcZdI8I";
+
+/*
+function updateMediaWindow(url){
+	document.getElementById("tubePlayer").setAttribute("src", url);
+}
+*/
